ff7e7e4452b6b1790d64601a57c2f741c3a8f688
Adam Alexander <aalexander@exacttarget.com>
2012-12-18 09:24:11 -0500

    Implement dynamic navigation

--- scripts/content.js
+++ scripts/content.js
@@ -4,9 +4,29 @@ define(function (require) {
 	var Handlebars = require('handlebars');
 	var $ = require('jquery');
 
-	var templates = {
-		step1: require('text!templates/step1.hbs')
-	};
+	var $navigation = $('#Navigation');
 
-    $('#PageContent').html(Handlebars.compile(templates.step1)());
+	function loadContent() {
+		// Determine content, defaulting to step1
+		if (!location.hash) location.hash = 'step1';
+		var content = location.hash.substring(1);
+
+		// Load template and apply it to the page with Handlebars
+		require(['text!templates/' + content + '.hbs'], function (template) {
+			$('#PageContent').html(Handlebars.compile(template)());
+		});
+
+		// Reconfigure navigation display
+		$navigation.find('li').removeClass('active');
+		$navigation.find('[data-content="' + content + '"]').addClass('active');
+	}
+
+	// Handle navigation clicks
+	$navigation.on('click', 'li', function () {
+		location.hash = $(this).data('content');
+	});
+
+	// Load content on hash change and page load
+	$(window).on('hashchange', loadContent);
+	loadContent();
 });
